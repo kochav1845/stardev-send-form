ad21de5afdf7c0dfba7565888a2fc6bdbd04d93a
kochav1845 <a88933513@gmail.com>
2025-02-25 18:17:23 -0500

    Add files via upload

---
+++ server.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const nodeMailer = require('nodemailer');
+app.use(express.static('public'));
+
+
+const PORT = process.env.PORT || 5000;
+
+// middleware
+
+app.use(express.json());
+app.use(cors());
+
+
+
+app.post('/',(req, res)=>{
+  
+  const transporter = nodeMailer.createTransport({
+    service: 'gmail',
+    auth:{
+      user: process.env.GMAIL_USER,
+      pass: process.env.GMAIL_PASS
+
+}});
+const mailOptions = {
+  from: 'a88933513@gmail.com',
+  replyTo: req.body.email,
+  to: 'a88933513@gmail.com',
+  subject: `Message from ${req.body.name}: ${req.body.email}`,
+  text: `Hi star dev, ${req.body.contact}, needs a ${req.body.type} his phone number is ${req.body.phone}he left a message: ${req.body.message}.`
+};
+transporter.sendMail(mailOptions, (error, info)=>{
+  if(error){
+    console.log(error);
+    res.send('error');
+  }else{
+      console.log('email sent')
+      res.send('success');
+  }})});
+
+app.listen(
+    PORT,()=>{
+        console.log(`server is runing on port ${PORT}`);
+       
+    }
+)
